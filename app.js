abaab9eb4184514cebe577b0d585ca26e883802a
Ivan <pokarayano@gmail.com>
2018-11-11 15:18:53 +0200

    pipes

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-//var http = require('http')
+var http = require('http')
 var fs = require('fs')
  //import fs from "fs"
-var myReadStream =fs.createReadStream(__dirname + '/readme.txt','utf8')
-var myWriteStream = fs.createWriteStream(__dirname + "/writeme.txt")
 
-myReadStream.on("data", function(chunk){
-    console.log("new chunk reCeived:");
-      myWriteStream.write(chunk)
-})
+//var myWriteStream = fs.createWriteStream(__dirname + "/writeme.txt")
 
+//myReadStream.pipe(myWriteStream)
 
-/*
 var server = http.createServer(function(req,res){
 
-    console.log('rqeust was made from  '+ req.url)
+    console.log('request was made from  '+ req.url)
     res.writeHead(200,{'Content-Type': 'text/plain'})
-    res.end('sssl');
+    var myReadStream =fs.createReadStream(__dirname + '/readme.txt','utf8')
+    myReadStream.pipe(res)
 });
 
 
 server.listen(3000,'127.0.0.1')
 
-console.log('server runs')*/
+console.log('server runs')
