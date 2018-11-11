399aa42b5572b9300a15a124b847e64729f338eb
Ivan <pokarayano@gmail.com>
2018-11-11 14:46:30 +0200

    readable stream

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var http = require('http')
+var fs = require('fs')
 
+var myReadStream =fs.createReadStream(__dirname + '/readme.txt','utf8')
+
+
+myReadStream.on("data", function(chunk){
+    console.log("new chunk reCeived:");
+    console.log(chunk)
+})
+
+
+/*
 var server = http.createServer(function(req,res){
 
     console.log('rqeust was made from  '+ req.url)
@@ -10,4 +21,4 @@ var server = http.createServer(function(req,res){
 
 server.listen(3000,'127.0.0.1')
 
-console.log('server runs')
+console.log('server runs')*/
