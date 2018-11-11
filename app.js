8fdc5fe8d877f0d618423a0d1094f0f2ad676b69
Ivan <pokarayano@gmail.com>
2018-11-11 14:57:48 +0200

    writeble streams

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var http = require('http')
+//var http = require('http')
 var fs = require('fs')
-
+ //import fs from "fs"
 var myReadStream =fs.createReadStream(__dirname + '/readme.txt','utf8')
-
+var myWriteStream = fs.createWriteStream(__dirname + "/writeme.txt")
 
 myReadStream.on("data", function(chunk){
     console.log("new chunk reCeived:");
-    console.log(chunk)
+      myWriteStream.write(chunk)
 })
 
 
