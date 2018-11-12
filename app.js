a03066cb45eee0ca1c0f9537535d16e946489397
Ivan <pokarayano@gmail.com>
2018-11-12 14:29:34 +0200

    ejs template engines

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express')
 
 var app = express();
+app.set('View engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('This is a main page.')
+ res.sendFile(__dirname+'/index.html')
 });
 
 app.get('/contact', (req, res) => {
-    res.send('This is the contact page.');
+  res.sendFile(__dirname+'/contact.html')
+});
+
+app.get('/users/:name', (req, res) => {
+    var data  ={
+        age:23,
+        job: 'ninja'
+    }
+   res.render(__dirname+'/views/profile.ejs',{person: req.params.name, data: data})
 });
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
+
